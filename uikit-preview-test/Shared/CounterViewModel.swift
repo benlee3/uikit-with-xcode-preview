846ebc4c89a5ce409135377789e06f3b8de2a766
Ben <bdl1123@gmail.com>
2021-11-22 00:15:46 -0500

    clean up

--- uikit-preview-test/Shared/CounterViewModel.swift
+++ uikit-preview-test/Shared/CounterViewModel.swift
@@ -11,16 +11,21 @@ import Foundation
 class CounterViewModel: ObservableObject {
     private var state: AppState
     private var cancellables = Set<AnyCancellable>()
-    // Mark fields that are bound to the View/ViewController as `@Published` so that we can subscribe to those
-    @Published private var count: Int = 0
     
-    var countLabelText: String {
-        return "Count: \(count)"
-    }
+    // Mark fields that are bound to the View/ViewController as `@Published` so that we can subscribe to any changes that occur to these fields via `objectWillChange`
+    // Marking this field as private because in this case we only want to expose the formatted string from the `countLabelText` property
+    @Published private var count: Int = 0
     
     init(state: AppState) {
         self.state = state
         // Assigning the field from state to a property on the ViewModel so that we don't have to expose the entire app state to the view controller
         state.$count.assign(to: \.count, on: self).store(in: &cancellables)
+    }
+}
+
+extension CounterViewModel {
+    // This is the public facing property that performs the "business logic" of what we need to do with the stored value (`count`) that we have
+    var countLabelText: String {
+        return "Count: \(count)"
     }
 }
